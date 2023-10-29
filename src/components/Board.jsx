51be5cbcbbc28ba0f1a648b2f62ca0cd3ca26b6c
= <=>
2023-10-29 17:58:27 -0400

    display as columns instead of individual cells

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,59 +5,14 @@ import cn from "classnames";
 function Board({ currentPlayer, currentTurnColor }) {
   const [redMoves, setRedMoves] = useState([]);
   const [yellowMoves, setYellowMoves] = useState([]);
-  // const columns = [
-  //   [1, 2, 3, 4, 5, 6],
-  //   [7, 8, 9, 10, 11, 12],
-  //   [13, 14, 15, 16, 17, 18],
-  //   [19, 20, 21, 22, 23, 24],
-  //   [25, 26, 27, 28, 29, 30],
-  //   [31, 32, 33, 34, 35, 36],
-  //   [37, 38, 39, 40, 41, 42],
-  // ];
-
-  const pieces = [
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
+  const columns = [
+    [1, 2, 3, 4, 5, 6],
+    [7, 8, 9, 10, 11, 12],
+    [13, 14, 15, 16, 17, 18],
+    [19, 20, 21, 22, 23, 24],
+    [25, 26, 27, 28, 29, 30],
+    [31, 32, 33, 34, 35, 36],
+    [37, 38, 39, 40, 41, 42],
   ];
 
   const clickColumn = (e) => {
@@ -70,15 +25,21 @@ function Board({ currentPlayer, currentTurnColor }) {
 
   return (
     <div className="board">
-      {pieces.map((piece, index) => {
+      {columns.map((column, index) => {
         return (
           <div
             key={index}
             onClick={clickColumn}
-            className="piece"
+            className="column"
             id={index + 1}
           >
-            {index + 1}
+            {column.map((piece, idx) => {
+              return (
+                <div key={idx} className="piece" id={piece}>
+                  {piece}
+                </div>
+              );
+            })}
           </div>
         );
       })}
