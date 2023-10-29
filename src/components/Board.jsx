ecde59bf9cd187ce9c25c782fee3b9eac391542b
= <=>
2023-10-29 14:59:44 -0400

    board styling and have just one array for pieces

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,29 +1,84 @@
 import React, { useState } from "react";
 import "../styles/main-menu.scss";
+import cn from "classnames";
 
-function Board() {
-  const columns = [
-    [1, 2, 3, 4, 5, 6],
-    [7, 8, 9, 10, 11, 12],
-    [13, 14, 15, 16, 17, 18],
-    [19, 20, 21, 22, 23, 24],
-    [25, 26, 27, 28, 29, 30],
-    [31, 32, 33, 34, 35, 36],
-    [37, 38, 39, 40, 41, 42],
+function Board({ currentPlayer, currentTurnColor }) {
+  const [redMoves, setRedMoves] = useState([]);
+  const [yellowMoves, setYellowMoves] = useState([]);
+  // const columns = [
+  //   [1, 2, 3, 4, 5, 6],
+  //   [7, 8, 9, 10, 11, 12],
+  //   [13, 14, 15, 16, 17, 18],
+  //   [19, 20, 21, 22, 23, 24],
+  //   [25, 26, 27, 28, 29, 30],
+  //   [31, 32, 33, 34, 35, 36],
+  //   [37, 38, 39, 40, 41, 42],
+  // ];
+
+  const pieces = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
   ];
+
+  const clickColumn = (e) => {
+    currentPlayer === 1 && setRedMoves([...redMoves, +e.target.id]);
+
+    currentPlayer === 2 && setYellowMoves([...yellowMoves, +e.target.id]);
+
+    e.target.style.background += "#FD6687";
+  };
 
   return (
     <div className="board">
-      {columns.map((column, index) => {
+      {pieces.map((piece, index) => {
         return (
-          <div key={index} className="column">
-            {column.map((piece) => {
-              return (
-                <div className="piece" id={piece}>
-                  {piece}
-                </div>
-              );
-            })}
+          <div
+            key={index}
+            onClick={clickColumn}
+            className="piece"
+            id={index + 1}
+          >
+            {index + 1}
           </div>
         );
       })}
